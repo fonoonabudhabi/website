b2565e558fc0fa81a865fc2ca5ebc219501335eb
mustafamoe95 <mustafamoe95@gmail.com>
2023-12-13 16:15:37 +0400

    fix social media links

--- src/components/social-media.tsx
+++ src/components/social-media.tsx
@@ -1,19 +1,24 @@
 import { cn } from "~/lib/cn";
 import { buttonVariants } from "./ui/button";
 import { Facebook, Instagram } from "lucide-react";
+import { directusServer, readSingleton } from "~/lib/directus";
 
-export default function SocialMedia(props: any) {
+export default async function SocialMedia() {
+    const data = await directusServer.request(
+        readSingleton("social_media", { fields: ["instagram", "facebook"] })
+    );
+
     return (
         <div className="flex items-center justify-center space-x-3">
             <a
-                href={props.instagram}
+                href={data.instagram}
                 target="_blank"
                 className={cn(buttonVariants({ size: "icon" }))}
             >
                 <Instagram className="w-4 h-4" />
             </a>
             <a
-                href={props.facebook}
+                href={data.facebook}
                 target="_blank"
                 className={cn(buttonVariants({ size: "icon" }))}
             >
