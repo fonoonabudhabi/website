05dc1c789a325773291ece4c4e6a1a776973b2ca
mustafamoe95 <mustafamoe95@gmail.com>
2023-12-23 14:41:14 +0400

    push image to top

--- src/components/background.tsx
+++ src/components/background.tsx
@@ -1,4 +1,3 @@
-import { colord } from "colord";
 import Image from "next/image";
 import { ASSETS_URL } from "~/lib/constants";
 import { directusServer, readSingleton } from "~/lib/directus";
@@ -8,15 +7,12 @@ export async function Background() {
         readSingleton("theme", { fields: ["*"] })
     );
 
-    // const primaryColor = colord(data.primaryColor).toHsl();
-    // const secondaryColor = colord(data.secondaryColor).toHsl();
-    // const textColor = colord(data.textColor).toHsl();
-
     return (
-        <div
-            className="fixed top-0 left-0 right-0 bottom-0 -z-10"
-            style={{ backgroundColor: data.background_color }}
-        >
+        <div className="fixed top-0 left-0 right-0 bottom-0 -z-10">
+            <div
+                className="absolute inset-0 -z-10"
+                style={{ backgroundColor: data.background_color }}
+            />
             <Image
                 src={`${ASSETS_URL}/${data.background}`}
                 alt=""
@@ -25,13 +21,5 @@ export async function Background() {
                 className="object-cover"
             />
         </div>
-        //     <style global="true">{`
-        //     :root {
-        //       --primary: ${primaryColor.h} ${primaryColor.s}% ${primaryColor.l}%;
-        //       --secondary: ${secondaryColor.h} ${secondaryColor.s}% ${secondaryColor.l}%;
-        //       --primary-foreground: ${textColor.h} ${textColor.s}% ${textColor.l}%;
-        //       --shadow: ${data.length_x}px ${data.length_y}px ${data.blur_radius}px ${data.spread_radius}px ${data.shadow_color};
-        //     }
-        //   `}</style>
     );
 }
